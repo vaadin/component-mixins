594134ea0b7ff77ffe60d2f0b4cf193e727ad74f
web-padawan <iamkulykov@gmail.com>
2019-12-09 11:28:13 +0200

    feat(test-helpers): mouse and touch helpers

--- packages/test-helpers/events.ts
+++ packages/test-helpers/events.ts
@@ -8,3 +8,15 @@ export function focusout(node: Element) {
   const event = new CustomEvent('focusout', { bubbles: true, composed: true });
   node.dispatchEvent(event);
 }
+
+export function mousedown(node: Element, buttons = 1) {
+  const event = new MouseEvent('mousedown', { bubbles: true, buttons });
+  node.dispatchEvent(event);
+}
+
+export function mouseup(node: Element) {
+  const event = new MouseEvent('mouseup', { bubbles: true });
+  node.dispatchEvent(event);
+}
+
+export { touchstart, touchend } from '@polymer/iron-test-helpers/mock-interactions.js';
